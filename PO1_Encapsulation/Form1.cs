4e303f1d4bafa070b3c0a79b6b10b1d235626e92
annema01 <123336005+annema01@users.noreply.github.com>
2024-02-08 11:41:05 -0500

    classe personnage + bouton attaque

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,24 +2,51 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new Personnage();
+        Personnage dragon = new Personnage();
+
         public Form1()
         {
             InitializeComponent();
+            //DateTime momentPresent = DateTime.Now;
+            // momentPresent.Year = 2025; erreur car objet DateTime a des propriétés en lecture seule
+
+            joueur.PointsDeVie = 10;
+            joueur.PointsDePuissance = 1;
+
+            dragon.PointsDeVie = 30; // ou joueur.PointeDeVie * 3;
+            dragon.PointsDePuissance = 3;
+
+        }
+
+        private void ActualiserLabels()
+        {
+            lbl_pointsVieJoueur.Text = "Vie du joueur : " + joueur.PointsDeVie.ToString();
+            lbl_pointsVieDragon.Text = "Vie du dragon : " + dragon.PointsDeVie.ToString();
+            lbl_puissanceAttaqueJoueur.Text = "Attaque du joueur : " + joueur.PointsDePuissance.ToString();
+            lbl_puissanceAttaqueDragon.Text = "Attaque du dragon : " + dragon.PointsDePuissance.ToString();
+
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
-
+            dragon.PointsDeVie -= joueur.PointsDePuissance;
+            ActualiserLabels();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+            ActualiserLabels();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            ActualiserLabels();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualiserLabels();
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage //remplace internal par oublic pour sauver du temps  //internal veut dire visible dans le même projet
+    {
+        string nom;
+        int pointsDeVie;
+        int pointsDePuissance;
+
+        //select les attributs, clic droit, Action rapide ... (premier) > encapsuler...(propriété)
+        // le propriété automatiquement nommé comme les attributs avec mais avec une lettre majuscule
+        public string Nom { get => nom; set => nom = value; } //si on supprime le set, la propriété sera en lecture seule
+        public int PointsDeVie
+        {
+            get => pointsDeVie;
+            set
+            {
+                if (value < 0)
+                {
+                    pointsDeVie = 0; //l'attribut ici
+                }
+                else
+                {
+                    pointsDeVie = value;
+                }
+            }
+        }
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+
+        public bool EstEnVie //dans la forme, on aurrait acces a faire une if (personnage.EstEnVie) "Vous êtes pas mort" par exemple
+        {
+            get
+            {
+                if (pointsDeVie > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+    }
+}
